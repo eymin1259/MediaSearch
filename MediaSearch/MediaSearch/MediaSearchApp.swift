ae9bc378af0ee45df184fc9aa1c3b237c545edf4
eymin1259 <eymin1259@gmail.com>
2022-12-12 22:47:20 +0900

    swiftUI 프로젝트 생성

---
+++ MediaSearch/MediaSearch/MediaSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaSearchApp.swift
+//  MediaSearch
+//
+//  Created by yongmin lee on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MediaSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
